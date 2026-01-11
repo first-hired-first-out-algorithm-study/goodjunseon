dcccb7e7bbaa81f8aa909a7c17d6efb09a5c1058
JunSeon Park <168715560+goodjunseon@users.noreply.github.com>
2026-01-11 18:44:01 +0900

    [Silver II] Title: 최소 힙, Time: 776 ms, Memory: 33728 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+
+            if (input == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.add(input);
+            }
+        }
+    }
+}
